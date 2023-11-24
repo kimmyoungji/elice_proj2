2a088fa23c745835c2e36f5a6b75dd8cf73d1cf1
[AI9] 최유림 <ul051101@gmail.com>
2023-11-24 18:42:05 +0900

    [유림] Fix: Navbar 수정

--- front/src/components/common/header/Navigation.js
+++ front/src/components/common/header/Navigation.js
@@ -1,26 +1,104 @@
 import React from 'react';
-import { Nav, Navbar, Container, Image } from "react-bootstrap";
+import { Nav, Navbar, Container, Image, Offcanvas } from "react-bootstrap";
 import logo from "./logo.png"
-
+//justify-content-end flex-grow-1 pe-3
 function Navigation() {
     return (
-        <Navbar className="bg-body-tertiary">
-            <Container className="navbar">
+        <Navbar expand="False" className="bg-body-tertiary mb-3">
+            <Container fluid>
               <Navbar.Brand href="/">
                 <Image src={logo} alt="logo" height="30" width="200"/>
               </Navbar.Brand>
-              <Nav>
-                <Nav.Link href="/">서비스 소개</Nav.Link>
-                <Nav.Link href="/community">커뮤니티</Nav.Link>
-                <Nav.Link href="/habits">나의 습관</Nav.Link>
-                <Nav.Link href="/userpage" className="ml-5">
+              <Nav className="d-flex flex-row ms-auto"> 
+                  <Nav.Link href="/" className="me-4">서비스 소개</Nav.Link>
+                  <Nav.Link href="/community" className="me-4">커뮤니티</Nav.Link>              
+                  <Nav.Link href="/habits" className="me-4">나의 습관</Nav.Link>
+              </Nav>
+              <Navbar.Toggle>
                 <Image src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
                     alt="profile" roundedCircle height="40" width="40"/>
-                </Nav.Link>
-              </Nav>
+              </Navbar.Toggle>
+              <Navbar.Offcanvas
+                    id="offcanvasNavbar-expand-false"
+                    aria-labelledby="offcanvasNavbarLabel-expand-false"
+                    placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id="offcanvasNavbarLabel-expand-false">
+                    <Image src={logo} alt="logo" height="30" width="200"/>
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link href="/userpage">내 정보</Nav.Link>
+                    <Nav.Link href="/">로그아웃</Nav.Link>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
             </Container>
         </Navbar>
     )
 }
 
 export default Navigation;
+
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Offcanvas from 'react-bootstrap/Offcanvas';
+
+// function Navigation() {
+//   return (
+//     <>
+//       {[false].map((expand) => (
+//         <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+//           <Container fluid>
+//             <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+//             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
+//             <Navbar.Offcanvas
+//               id={`offcanvasNavbar-expand-${expand}`}
+//               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+//               placement="end"
+//             >
+//               <Offcanvas.Header closeButton>
+//                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+//                   Offcanvas
+//                 </Offcanvas.Title>
+//               </Offcanvas.Header>
+//               <Offcanvas.Body>
+//                 <Nav className="justify-content-end flex-grow-1 pe-3">
+//                   <Nav.Link href="#action1">Home</Nav.Link>
+//                   <Nav.Link href="#action2">Link</Nav.Link>
+//                   {/* <NavDropdown
+//                     title="Dropdown"
+//                     id={`offcanvasNavbarDropdown-expand-${expand}`}
+//                   >
+//                     <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+//                     <NavDropdown.Item href="#action4">
+//                       Another action
+//                     </NavDropdown.Item>
+//                     <NavDropdown.Divider />
+//                     <NavDropdown.Item href="#action5">
+//                       Something else here
+//                     </NavDropdown.Item>
+//                   </NavDropdown> */}
+//                 </Nav>
+//                 {/* <Form className="d-flex">
+//                   <Form.Control
+//                     type="search"
+//                     placeholder="Search"
+//                     className="me-2"
+//                     aria-label="Search"
+//                   />
+//                   <Button variant="outline-success">Search</Button>
+//                 </Form> */}
+//               </Offcanvas.Body>
+//             </Navbar.Offcanvas>
+//           </Container>
+//         </Navbar>
+//       ))}
+//     </>
+//   );
+// }
+
+// export default Navigation;
