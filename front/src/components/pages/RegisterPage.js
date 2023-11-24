4e37c1aace036435ac2dc8a1142ad3d85c24f3df
[AI9] 전성혜01~ <dvlpsh99@gmail.com>
2023-11-24 19:48:28 +0900

    [성혜] Feat: 회원가입 페이 구현 1차

---
+++ front/src/components/pages/RegisterPage.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import RegisterForm from "../features/Register/RegisterForm";
+
+const Register = () => {
+  return (
+    <BrowserRouter>
+      <RegisterForm />
+    </BrowserRouter>
+  );
+};
+
+export default Register;
