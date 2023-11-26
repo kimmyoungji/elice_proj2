e44bb2e58614c0c44579daf89db16681eb94daab
[AI9] 전성혜01~ <dvlpsh99@gmail.com>
2023-11-26 20:16:43 +0900

    [성혜] 회원가입 페이지 구현 2차

--- front/src/components/features/Register/RegisterForm.js
+++ front/src/components/features/Register/RegisterForm.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { Container, Col, Row, Form, Button } from "react-bootstrap";
-import api from "./axiosConfig";
+import axios from "axios";
 import { validateEmail, validateNickname, validatePassword } from "./validate";
-import { duplicateCheck } from "./duplicateCheck";
 
 function RegisterForm() {
   const navigate = useNavigate();
@@ -13,6 +12,8 @@ function RegisterForm() {
   const [password, setPassword] = useState("");
   const [confirmPwd, setConfirmPwd] = useState("");
   const [nickname, setNickname] = useState("");
+  const [idErrMsg, setIdErrMsg] = useState("");
+  const [idCheckMsg, setIdCheckMsg] = useState("");
 
   //이메일 형태 적합 여부 확인
   const isEmailValid = validateEmail(email);
@@ -27,101 +28,130 @@ function RegisterForm() {
   const isFormValid =
     isEmailValid && isPasswordValid && isPasswordSame && isNicknameValid;
 
-  const handleSubmit = async (e) => {
+  // 이메일 중복검사
+  const onClickRegister = async (e) => {
     e.preventDefault();
+    await axios.get("user/register").then((res) => {
+      const resMessage = res.data.message;
+      if (resMessage === "SUCCESS") {
+        setIdErrMsg("");
+        setIdCheckMsg("사용 가능한 아이디입니다.");
+      } else if (resMessage === "FAIL") {
+        setIdErrMsg("이미 사용중인 아이디입니다.");
+        setIdCheckMsg("");
+      }
+    });
+    // 닉네임 중복검사
+    await axios.get("user/register").then((res) => {
+      const resMessage = res.data.message;
+      if (resMessage === "SUCCESS") {
+        setIdErrMsg("");
+        setIdCheckMsg("사용 가능한 닉네임입니다.");
+      } else if (resMessage === "FAIL") {
+        setIdErrMsg("이미 사용중인 닉네임입니다.");
+        setIdCheckMsg("");
+      }
+    });
 
-    try {
-      // "user/register" 엔드포인트로 post요청함.
-      await api.post("user/register", {
-        email,
-        password,
-        nickname,
-      });
+    const handleSubmit = async (e) => {
+      e.preventDefault();
 
-      // 로그인 페이지로 이동함.
-      navigate("/login");
-    } catch (err) {
-      console.log("회원가입에 실패하였습니다.", err);
-    }
+      try {
+        // "user/register" 엔드포인트로 post요청함.
+        await axios.post("user/register", {
+          email,
+          password,
+          nickname,
+        });
+
+        // 로그인 페이지로 이동함.
+        navigate("/login");
+      } catch (err) {
+        console.log("회원가입에 실패하였습니다.", err);
+      }
+    };
+
+    return (
+      <Container>
+        <Row className="justify-content-md-center mt-5">
+          <Col lg={8}>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="registerEmail">
+                <Form.Label>이메일 주소</Form.Label>
+                <Form.Control
+                  type="email"
+                  autoComplete="off"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {!isEmailValid && (
+                  <Form.Text className="text-success">
+                    이메일 형식이 올바르지 않습니다.
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              <Form.Group controlId="registerPassword" className="mt-3">
+                <Form.Label>비밀번호</Form.Label>
+                <Form.Control
+                  type="password"
+                  autoComplete="off"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {!isPasswordValid && (
+                  <Form.Text className="text-success">
+                    비밀번호는 5글자 이상부터 설정 가능합니다.
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              <Form.Group controlId="registerConfirmPassword" className="mt-3">
+                <Form.Label>비밀번호 확인</Form.Label>
+                <Form.Control
+                  type="password"
+                  autoComplete="off"
+                  value={confirmPwd}
+                  onChange={(e) => setConfirmPwd(e.target.value)}
+                />
+                {!isPasswordSame && (
+                  <Form.Text className="text-success">
+                    비밀번호가 일치하지 않습니다.
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              <Form.Group controlId="registerName" className="mt-3">
+                <Form.Label>닉네임</Form.Label>
+                <Form.Control
+                  type="text"
+                  autoComplete="off"
+                  value={nickname}
+                  onChange={(e) => setNickname(e.target.value)}
+                />
+                {!isNicknameValid && (
+                  <Form.Text className="text-success">
+                    2~8자로 설정 가능합니다.
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              <Form.Group as={Row} className="mt-3 text-center">
+                <Col sm={{ span: 20 }}>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    disabled={!isFormValid}
+                  >
+                    회원가입
+                  </Button>
+                </Col>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
   };
-
-  return (
-    <Container>
-      <Row className="justify-content-md-center mt-5">
-        <Col lg={8}>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="registerEmail">
-              <Form.Label>이메일 주소</Form.Label>
-              <Form.Control
-                type="email"
-                autoComplete="off"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {!isEmailValid && (
-                <Form.Text className="text-success">
-                  이메일 형식이 올바르지 않습니다.
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group controlId="registerPassword" className="mt-3">
-              <Form.Label>비밀번호</Form.Label>
-              <Form.Control
-                type="password"
-                autoComplete="off"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {!isPasswordValid && (
-                <Form.Text className="text-success">
-                  비밀번호는 5글자 이상부터 설정 가능합니다.
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group controlId="registerConfirmPassword" className="mt-3">
-              <Form.Label>비밀번호 확인</Form.Label>
-              <Form.Control
-                type="password"
-                autoComplete="off"
-                value={confirmPwd}
-                onChange={(e) => setConfirmPwd(e.target.value)}
-              />
-              {!isPasswordSame && (
-                <Form.Text className="text-success">
-                  비밀번호가 일치하지 않습니다.
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group controlId="registerName" className="mt-3">
-              <Form.Label>닉네임</Form.Label>
-              <Form.Control
-                type="text"
-                autoComplete="off"
-                value={nickname}
-                onChange={(e) => setNickname(e.target.value)}
-              />
-              {!isNicknameValid && (
-                <Form.Text className="text-success">
-                  2~8자로 설정 가능합니다.
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group as={Row} className="mt-3 text-center">
-              <Col sm={{ span: 20 }}>
-                <Button variant="primary" type="submit" disabled={!isFormValid}>
-                  회원가입
-                </Button>
-              </Col>
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
 }
-
 export default RegisterForm;
