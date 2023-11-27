5a421d7d5a283bc39da98c96af8c660f9f48f5be
[AI9] 최유림 <ul051101@gmail.com>
2023-11-27 10:19:24 +0900

    [유림] Fix: habit page 구현 2차

--- front/src/components/features/habit/HabitPageForm.js
+++ front/src/components/features/habit/HabitPageForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { Card, Container, Button, Col, Row, Dropdown, ListGroup } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { Card, Container, Button, Col, Row, Dropdown, ListGroup, Image } from 'react-bootstrap';
 import './HabitPage.css';
+import calendar from "../../../assets/imgs/calendar.png";
 
 
 export function HabitForm ({ userInfo, habitList }) {
@@ -9,6 +11,8 @@ export function HabitForm ({ userInfo, habitList }) {
     const [select, setSelect] = useState(false);
     // const [selectedHabit, setSelectedHabit] = useState(null);
     const habits = habitList
+
+    const navigate = useNavigate();
 
     const getTurtleImage = (level) => {
         const turtleImg = require(`../../../assets/imgs/거북이${level}.png`);
@@ -39,36 +43,41 @@ export function HabitForm ({ userInfo, habitList }) {
 
     return (
         <>
-            <Container className="habits-container" style={{ marginTop: '50px' }}>
-                <Container className="turtle-container">
-                    <Card style={{ height: '400px' }}>
-                        <Card.Body>
-                            <Card.Title>{userName}의 거북잉</Card.Title>
-                        </Card.Body>
-                        {turtleLevel >= 1 && turtleLevel <= 5 && getTurtleImage(turtleLevel)}          
-                    </Card>
-                </Container>
-                <Container className="habit-container">
-                    <Card style={{ height: '400px' }}>
-                        <Card.Body>
-                            <Card.Title style={{ marginBottom: '150px' }}>{userName}의 습관</Card.Title>
-                            <Card.Body className="d-flex justify-content-center align-items-center">
-                                {add === false && <Button variant="primary" size="lg" onClick={() => addButton()}>+</Button>}
-                                {add === true && select === true && <ListGroup>{habitClick}</ListGroup>}
-                                {add === true &&
-                                <Dropdown.Menu show style={{ width: '100%' }}>
-                                    <Dropdown.Header>아래에서 습관을 선택하세요</Dropdown.Header>
-                                    {getHabitList}
-                                </Dropdown.Menu>}
+            <Container className="habits-container" style={{ marginTop: '50px'}}>
+                <Row>
+                    <Col xs={12} className="d-flex justify-content-end">
+                        <Image src={calendar} alt="Calendar image" style={{ width: '5%' }} onClick={() => navigate('/calendar')}/>
+                    </Col>
+                    <Col className="turtle-container">
+                        <Card style={{ height: '400px' }}>
+                            <Card.Body>
+                                <Card.Title>{userName}의 거북잉</Card.Title>
                             </Card.Body>
-                        </Card.Body>
-                    </Card>
-                </Container>
+                            {turtleLevel >= 1 && turtleLevel <= 5 && getTurtleImage(turtleLevel)}          
+                        </Card>
+                    </Col>
+                    <Col className="habit-container">
+                        <Card style={{ height: '400px' }}>
+                            <Card.Body>
+                                <Card.Title style={{ marginBottom: '150px' }}>{userName}의 습관</Card.Title>
+                                <Card.Body className="d-flex justify-content-center align-items-center">
+                                    {add === false && <Button variant="primary" size="lg" onClick={() => addButton()}>+</Button>}
+                                    {add === true && select === true && <ListGroup>{habitClick}</ListGroup>}
+                                    {add === true &&
+                                    <Dropdown.Menu show style={{ width: '100%' }}>
+                                        <Dropdown.Header>아래에서 습관을 선택하세요</Dropdown.Header>
+                                        {getHabitList}
+                                    </Dropdown.Menu>}
+                                </Card.Body>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row> 
             </Container>     
         </>
     )
 };
-//<Card.Text style={{ color: 'black' }}>???</Card.Text>
+
 
 export function HabitListForm (habitList) {
     const turtleImg = require(`../../../assets/imgs/거북이1.png`);
@@ -83,7 +92,7 @@ export function HabitListForm (habitList) {
     const randomIndex = Math.floor(Math.random() * cheerUpTexts.length);
 
     const cards = Object.keys(habits).map((key) => (
-        <Col key={key} xs={12} md={6} lg={4} className="mb-4">
+        <Col key={key} xs={12} md={6} lg={4} className="mb-4" style={{ marginRight: '0'}}>
             <Card border="light" style={{ width: '18rem' }}>
                 <Card.Body>
                     <Card.Img src={turtleImg} alt="Card image" />
