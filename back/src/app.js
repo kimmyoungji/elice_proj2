6b804bff817f1137b5a296b1051ef859a689777c
HY 'Yenny' Kim <o2hykim@gmail.com>
2023-11-29 20:24:53 +0900

    fix: 미완성 부분 주석처리

--- back/src/app.js
+++ back/src/app.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const introRouter = require("./routers/introRouter");
-const calenderRouter = require("./routers/calenderRouter");
+// const calenderRouter = require("./routers/calenderRouter");
 
 const app = express();
 const corsOptions = {
@@ -15,10 +15,20 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
+
+const connectDB = require("./middlewares/connectDB");
+app.get("/", connectDB, async (req, res, next) => {
+  try {
+    await req.connectDB;
+    res.send("turtine에 오신걸 환영합니다 야호");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+});
 
 app.use("/graphs", introRouter);
-app.use("/calender", calenderRouter);
+// app.use("/calender", calenderRouter);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(
