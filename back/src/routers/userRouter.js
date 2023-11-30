93b3a7fd3b305b40ac08ecb1ebc3ec2e53f2f04b
[AI9] 김명지 <kmjoyit21045@gmail.com>
2023-11-30 12:28:26 +0900

    :wrench: Fix: userMVP /users/login 라우터 응답 형식 변경

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -13,13 +13,16 @@ userRouter.post("/users/login", async (req, res, next) => {
   try {
     // 요청 바디 데이터 받아오기
     const { email, password } = req.body;
+
     // DB: user_id로 유저 조회
     const result = await users.findByEmail(email);
     const user = result[0];
+
     // user 유무 검증
     if (!user) {
       throw new INVALID_USER_Error("등록되지 않은 이메일입니다.");
     }
+
     // password 검증
     const correctPasswordHash = user.password;
     const isPasswordCorrect = await bcrypt.compare(
@@ -29,6 +32,7 @@ userRouter.post("/users/login", async (req, res, next) => {
     if (!isPasswordCorrect) {
       throw new INVALID_USER_Error("잘못된 비밀번호입니다.");
     }
+
     // jwt 생성
     const accessToken = await jwt.sign(
       { user_id: user.user_id, username: user.username, email: user.email },
@@ -38,6 +42,7 @@ userRouter.post("/users/login", async (req, res, next) => {
         expiresIn: 60 * 60 * 24, // 유효기간
       }
     );
+
     // cookie 전송
     const cookieOption = {
       path: "/",
@@ -48,8 +53,12 @@ userRouter.post("/users/login", async (req, res, next) => {
     res.cookie("accessToken", accessToken, cookieOption);
     // 응답
     res.status(200).send({
-      statusCode: 200,
       message: "로그인 성공",
+      user: {
+        user_id: user.user_id,
+        username: user.username,
+        email: user.email,
+      },
     });
 
     // OR
@@ -64,12 +73,15 @@ userRouter.get("/users/user", isLoggedIn, async (req, res, next) => {
   try {
     // 요청 쿠키 데이터 가져오기
     const user_id = req.currentUserId;
+
     // DB:  user_id로 users 조회
     const result = await users.findById(user_id);
+
     // 빈 배열일 경우 에러처리
     if (result.length === 0) {
       throw new NotFoundError("데이터가 존재하지 않습니다.");
     }
+
     // 응답 데이터 구성하기
     delete result[0].password;
 
@@ -88,6 +100,7 @@ userRouter.get("/users", async (req, res, next) => {
   try {
     // DB: 모든 users 조회
     let userArr = await users.findById();
+
     // 응답 데이터 구성하기
     userArr = userArr.map((user) => {
       delete user.password;
@@ -114,6 +127,7 @@ userRouter.post("/users", async (req, res, next) => {
   try {
     // 요청 바디 데이터 가져오기
     const { username, email, password } = req.body;
+
     // 응답 데이터 구성하기
     const user_id = uuidv4();
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -124,6 +138,7 @@ userRouter.post("/users", async (req, res, next) => {
       password: hashedPassword,
       level: 0,
     };
+
     // DB: user 데이터 DB에 추가하기
     const result = await users.create(newUser); //왜 반환 값이 0이 나올까?..
 
@@ -140,6 +155,7 @@ userRouter.post("/users", async (req, res, next) => {
 userRouter.delete("/users", isLoggedIn, async (req, res, next) => {
   try {
     // cookie에서 user_id 받아오기
+
     const user_id = req.currentUserId;
     // DB: db 데이터 삭제
     const result = await users.deleteById(user_id);
@@ -160,6 +176,7 @@ userRouter.put("/users", isLoggedIn, async (req, res, next) => {
     // 요청 쿠키, 바디에서 값 받아오기
     const user_id = req.currentUserId;
     const toUpdate = { ...req.body };
+
     // DB: DB 데이터 수정하기
     const result = await users.update(user_id, toUpdate);
 
