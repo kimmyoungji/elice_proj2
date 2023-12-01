b4534fd607d4b1ca88c547042e5b4969aec719e8
[AI9] 최유림 <ul051101@gmail.com>
2023-12-01 22:05:49 +0900

    [유림] Fix: habitpage api 수정

--- front/src/components/features/habit/HabitPageForm.js
+++ front/src/components/features/habit/HabitPageForm.js
@@ -1,26 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Card, Container, Button, Col, Row, ListGroup, Image, Form } from 'react-bootstrap';
 import './HabitPage.css';
 import calendar from "../../../assets/imgs/calendar.png";
 import axios from 'axios';
 
-
-export default function HabitForm ({ userInfo, habitList }) {
+// , selectedHabits
+export default function HabitForm ({ userInfo, habitList, selectedHabits }) {
     const { userName, turtleLevel } = userInfo;
     const habits = habitList;
+    // const selectedHabits = ["habit1", "habit2"];
 
     return (
         <>
             <Container className="habits-container">
                 <Row>
                     <TurtleForm userName={userName} turtleLevel={turtleLevel}/>
-                    <HabitAddForm userName={userName} habits={habits}/>
-                </Row> 
-            </Container>     
+                    
+                    {/* 기존에 선택한 습관이 없는 경우  */}
+                    {!selectedHabits && <HabitAddForm
+                                            userName={userName}
+                                            habits={habits}/>}
+                    {/* 기존에 선택한 습관이 있는 경우  */}
+                    {selectedHabits && <HabitShowForm
+                                            userName={userName}
+                                            selectedHabits={selectedHabits}
+                                            request={true}/>}
+                   
+                    {/* <HabitCardForm userName={userName}
+                                   habits={habits}
+                                   selectedHabits={selectedHabits}/> */}
+                </Row>
+            </Container>
         </>
     )
 };
+
 
 const TurtleForm = ({ userName, turtleLevel }) => {
     const navigate = useNavigate();
@@ -56,12 +71,21 @@ const TurtleForm = ({ userName, turtleLevel }) => {
     )
 }
 
-
+// const HabitCardForm = ({ userInfo, habitList, selectedHabits }) => {
+//     return (
+//         <>
+//             {!selectDone && <HabitAddForm userName={userName}
+//                                             habits={habits}/>}
+//             {selectDone && <HabitShowForm selectedHabits={selectedHabits} request={false}/>}
+//         </>
+//     )
+// }
 
 const HabitAddForm = ({ userName, habits }) => {
     const [ addButton, setAddButton ] = useState(true);
     const [ selectedHabit, setSelectedHabit ] = useState([]);
     const [ selectedDay, setSelectedDay ] = useState(null);
+    const [ selectDone, setSelectDone ] = useState(false);
 
     const handleAddButton = () => {
         setAddButton(false);
@@ -102,15 +126,34 @@ const HabitAddForm = ({ userName, habits }) => {
     const handleSelectButton = () => {
         console.log(selectedHabit);
         console.log(selectedDay);
-        axios.post("http://"+ window.location.hostname +":5001/planned-habits",
-            selectedHabit)
+
+        // 새롭게 선택한 습관 추가하기
+        // 아직 api 연결 x -> 백에서 변수와 data 수정중
+        axios({
+            method: 'post',
+            url: "http://"+ window.location.hostname +":5001/planned_habits",
+            withCredentials: true,
+            headers: {
+            "Content-Type": "application/json",
+            },
+            data: {
+                selectedHabit: selectedHabit,
+                selectedDay: selectedDay
+            }
+        })
         .then((res) => {
-            console.log(res)
+            // 백에 카멜케이스로 수정 요청
+            const { habit_ids } = res.data.plannedHabits[0];
+            console.log(habit_ids);
         }).catch((error) => {
+            // 추후 수정예정
             console.log(error)
         }).then(() => {
         });
+
+        setSelectDone(true);
     }
+
 
     return (
         <>
@@ -124,19 +167,19 @@ const HabitAddForm = ({ userName, habits }) => {
                         <div style={{ color: "grey", marginBottom: '20px', fontSize: "80%" }}>
                             습관과 실천 기간을 추가해보세요 !
                         </div>
-                        {addButton === true &&
+                        {addButton &&
                         <div className="d-flex justify-content-center" style={{ marginTop: '100px' }}>
                             <Button variant="primary" size="lg"
                             onClick={() => handleAddButton()}>+
                             </Button>
                         </div>
                         }
-                        {addButton === false &&
+                        {!addButton &&
                         <ListGroup style={{ position: 'relative', width: '100%', fontSize: "83%"}}>
                             {getHabitList}
                         </ListGroup>}
                     </Card.Body>
-                    {addButton === false  &&
+                    {!addButton  &&
                     <>
                         <ListGroup horizontal="sm"
                             className="d-flex justify-content-center"
@@ -153,6 +196,72 @@ const HabitAddForm = ({ userName, habits }) => {
                     </>}
                 </Card>
             </Col>
+            {/* {selectDone && <HabitShowForm selectedHabits={selectedHabit} request={false}/>} */}
         </>
     )
 }
+
+// 선택한 습관들 조회하기
+const HabitShowForm = ({ userName, selectedHabits, request}) => {
+    console.log(request);
+    console.log(selectedHabits);
+    const [ checkHabit, setCheckHabit ] = useState(false);
+
+    const getDoneHabit = () => {
+        setCheckHabit(true)
+        
+        return (
+            <></>
+        )
+    }
+
+    useEffect(() => {
+        axios({
+          method: 'get',
+          url: "http://"+ window.location.hostname +":5001/fullfilled_habits",
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          }
+        })
+        .then((res) => {
+          // 백에 카멜케이스로 요청
+            const { habit_id } = res.data.fullfilledHabits[0];
+          console.log(habit_id);
+        }).catch((error) => {
+          // 추후 수정예정
+            console.log(error)
+        }).then(() => {
+        });
+      }, [checkHabit])
+
+    
+    return (
+        <>
+            <Col xs={12} sm={6} className="habit-container" >
+                <Card style={{ height: '450px' }}>
+                    <Card.Body style={{ height: "100%" }}>
+                        <Card.Title>
+                            <span style={{ fontSize: "30px" }}>
+                                {userName}</span>의 습관
+                        </Card.Title>
+                        {!request && <ListGroup style={{ position: 'relative', width: '100%', fontSize: "83%"}}>
+                            {selectedHabits}
+                        </ListGroup>}
+                        {request && <ListGroup style={{ position: 'relative', width: '100%', fontSize: "83%"}}>
+                            {getDoneHabit}
+                        </ListGroup>}
+                    </Card.Body>
+                        <div className="d-flex justify-content-center">
+                            <Button className="select-button" variant="primary" size="lg"
+                                // onClick={() => handleSelectButton()}
+                                style={{ width: "30%", fontSize: '13px', margin: "10px"}}>
+                                    실천완료
+                            </Button>
+                        </div>
+                </Card>
+            </Col>
+        </>
+    )
+}
+
--- front/src/components/pages/HabitPage.js
+++ front/src/components/pages/HabitPage.js
@@ -6,31 +6,38 @@ import axios from 'axios';
 
 export default function HabitPage() {
   const [isEditing, setIsEditing] = useState(true);
+  const [selectedHabits, setSelectedHabits] = useState();
 
-  // 페이지 처음 로드되면 사용자의 완료 습관 조회하기
-  //     api.get("/fullfilled_habits", {
+  // 페이지 처음 로드되면 사용자의 계획 습관 조회하기
+  //     api.get("/planned-habits", {
   // useEffect(() => {
   //     
   //     });
   // })
 
+  // 사용자가 계획한 습관이 있는지 확인
+  // if yes -> 계획한 습관 띄우기
+  // if no -> 추가하기 버튼 띄우기
   useEffect(() => {
     axios({
       method: 'get',
-      url: "http://"+ window.location.hostname +":5001/fullfilled_habits",
+      url: "http://"+ window.location.hostname +":5001/planned-habits",
       withCredentials: true,
       headers: {
         "Content-Type": "application/json",
       }
     })
     .then((res) => {
-        const { fullfilled_habit_id, habit_id } = res.data;
-      console.log(res);
+      // 백에 카멜케이스로 수정 요청함
+      const { habitIds } = res.data.plannedHabits[0];
+      setSelectedHabits(habitIds);
     }).catch((error) => {
+      // 추후 수정예정
         console.log(error)
     }).then(() => {
     });
-}, [])
+  }, [])
+
 
   const userInfo = {
     userName : "거북잉",
@@ -48,7 +55,10 @@ export default function HabitPage() {
 
   return (
     <>
-      <HabitForm userInfo={userInfo} habitList={habitList} />
+      <HabitForm
+        userInfo={userInfo}
+        habitList={habitList}
+        selectedHabits={selectedHabits} />
       {isEditing === true && <HabitListForm habitList={habitList} />}
     </>
   );
