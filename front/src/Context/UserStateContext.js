631eaee656db57ea4825fc1059e99424358a157c
dev-vming <dev.vming@gmail.com>
2023-12-05 22:11:17 +0900

    Feat: 로그인 정보 기본값 수정

--- front/src/Context/UserStateContext.js
+++ front/src/Context/UserStateContext.js
@@ -1,11 +1,13 @@
 import { createContext, useReducer } from "react";
 import LoginReducer from "./LoginReducer";
 
-export const UserStateContext = createContext(JSON.parse(localStorage.getItem('user')));
+let user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : "";
+
+export const UserStateContext = createContext(user);
 export const UserDispatchContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
-  const [userState, dispatch] = useReducer(LoginReducer, { user: null });
+  const [userState, dispatch] = useReducer(LoginReducer, user);
 
   return (
     <UserStateContext.Provider value={userState}>
