fde306706f229193dd82ab88e4afbcff3086ec7e
[AI9] 최유림 <ul051101@gmail.com>
2023-12-05 11:12:38 +0900

    [유림] Fix: 병합 오류 해결

--- back/src/lib/cookieOption.js
+++
@@ -1,8 +0,0 @@
-const cookieOption = {
-  path: "/",
-  httpOnly: true,
-  sameSite: "lax",
-  maxAge: 14 * 24 * 60 * 60 * 1000,
-};
-
-module.exports = cookieOption;
--- back/src/lib/stringToarray.js
+++
@@ -1,9 +0,0 @@
-function stringToArr(str) {
-  if (str.startsWith("[") && str.endsWith("]")) {
-    let arr = str.trim().replace("[", "").replace("]", "").split(",");
-    return (arr = arr.map((elm) => elm.trim()));
-  }
-  throw new Error("변환하고자 하는 문자열의 형식이 배열의 형식이 아닙니다");
-}
-
-module.exports = { stringToArr };
