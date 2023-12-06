80c0572e4fdb2dcb683251f4bb82fcd4224aa8bc
[AI9] 최유림 <ul051101@gmail.com>
2023-12-06 11:39:48 +0900

    [유림] Fix: 충돌 수정

--- front/src/components/features/habit/HabitPageForm.js
+++ front/src/components/features/habit/HabitPageForm.js
@@ -8,10 +8,6 @@ import calendar from "../../../assets/imgs/calendar.png";
 export default function HabitForm ({ userInfo, habitList, selectedDate, selectedHabits }) {
     const { userName, turtleLevel } = userInfo;
     
-<<<<<<< HEAD
-
-=======
->>>>>>> dev-front
     return (
         <>
             <Container className="habits-container">
@@ -26,8 +22,6 @@ export default function HabitForm ({ userInfo, habitList, selectedDate, selected
         </>
     )
 };
-<<<<<<< HEAD
-
 
 const TurtleForm = ({ userName, turtleLevel }) => {
     const navigate = useNavigate();
@@ -62,45 +56,4 @@ const TurtleForm = ({ userName, turtleLevel }) => {
         </>
     )
 }
-
-
-=======
-
-
-const TurtleForm = ({ userName, turtleLevel }) => {
-    const navigate = useNavigate();
-
-    const getTurtleImage = (level) => {
-        const turtleImg = require(`../../../assets/imgs/거북이${level}.png`);
-        return (
-            <Card.Body className="d-flex justify-content-center align-items-center">
-                <Card.Img src={turtleImg} alt="Card image" style={{ width: '70%'}}/>
-            </Card.Body>
-        )
-    };
-    return (
-        <>
-            <Col xs={12} className="d-flex justify-content-end">
-                <Image src={calendar} alt="Calendar image"
-                    style={{ width: '5%' }}
-                    onClick={() => navigate('/calendar')}/>
-            </Col>
-            <Col xs={12} sm={6} className="turtle-container">
-                <Card style={{ height: '450px' }}>
-                    <Card.Body>
-                        <Card.Title>
-                            <span style={{ fontSize: "30px" }}>
-                                {userName}</span>의 거북잉
-                        </Card.Title>
-                    </Card.Body>
-                    {turtleLevel >= 1 && turtleLevel <= 5
-                    && getTurtleImage(turtleLevel)}          
-                </Card>
-            </Col>
-        </>
-    )
-}
->>>>>>> dev-front
-
-
 
