fd6d4e83d9b26a600dadbb09305ca939d25ae0ce
[AI9] 최유림 <ul051101@gmail.com>
2023-12-07 22:26:55 +0900

    [유림] 로그인 에러 수정

--- front/src/App.js
+++ front/src/App.js
@@ -29,7 +29,8 @@ export default function App() {
     if (location.pathname === "/register" || location.pathname === "/login") return;
     api.get("/users/user")
       .then((res) => {
-        const data = res.user[0];
+        const data = res.user;
+        console.log(res);
         setUser(data);
       })
       .catch(() => {
